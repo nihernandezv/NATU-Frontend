f51276740057ba593cf842c5827e7cdc1b0c5b37
Davidrg02 <davrodriguezgu@unal.edu.co>
2024-03-23 11:53:03 -0500

    footer

--- src/Pages/Layout/Layout.jsx
+++ src/Pages/Layout/Layout.jsx
@@ -4,27 +4,17 @@ import "./Layout.css";
 export default function Layout(){
 
   return (
-    <>{/*
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Natu</Link>
-          </li>
-          <li>
-            <Link to="/login">Inicio de sesión</Link>
-          </li>
-          <li>
-            <Link to="/register">Registro</Link>
-          </li>
-        </ul>
-      </nav>*/}
-
+    <>
+      {/* Navbar */}
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
           <Link to="/" className="navbar-brand">
-            <img src="/logo.png" class="img-fluid" alt="Bootstrap" width="100" height="24"/>
+            <img src="/logo.png" alt="Logo" className="img-fluid" width="100" height="24"/>
           </Link>
-          <div className="collapse navbar-collapse">
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link to="/login" className="nav-link">Inicio de sesión</Link>
@@ -39,9 +29,34 @@ export default function Layout(){
 
       <Outlet />
 
-      <footer>
-        <p>Footer</p>
+      {/* Footer */}
+      <footer className="bg-dark text-center text-white">
+        <div className="container p-4">
+          {/* Sección de redes sociales */}
+          <section className="mb-4">
+              {/* Ejemplo de botón de Facebook */}
+              <a className="btn btn btn-floating m-1" href="https://facebook.com" role="button">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/2048px-2021_Facebook_icon.svg.png" alt="Bootstrap" className="img-fluid" width="50" height="24"/>
+              </a>
+              {/* Agrega más botones de redes sociales aquí */}
+          </section>
+
+          {/* Sección de texto */}
+          <section className="mb-4">
+              <p>
+                  Aquí puedes usar filas y columnas para organizar tu pie de página. Bootstrap's grid system
+                  te permite crear un layout adaptativo y responsivo fácilmente.
+              </p>
+          </section>
+        </div>
+
+        {/* Derechos de autor */}
+        <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+            © 2024 Derechos reservados:
+            <a className="text-white" href="https://tusitioweb.com/">TuSitioWeb.com</a>
+        </div>
       </footer>
+
     </>
   )
 };
