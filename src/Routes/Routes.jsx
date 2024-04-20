8903532a1ec00c0c11aa656b1bac9a22c0ed5616
nihernandezv <nihernandezv@unal.edu.co>
2024-04-20 18:25:47 -0500

    rutas vendor

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -6,6 +6,10 @@ import Login from '../Pages/Login/Login.jsx';
 import Layout from '../Pages/Layout/Layout.jsx';
 import Reset from '../Pages/Recover/Reset.jsx';
 import Catalog from '../Pages/Home/ProductosVista.jsx';
+import Vendor from '../Pages/Vendor/Vendor.jsx';
+import VendorProduct from '../Pages/Vendor/Products/Products.jsx';
+import VendorProfile from '../Pages/Vendor/Products/Profile.jsx'; 
+import VendorTransactions from '../Pages/Vendor/Transactions/Transactions.jsx';
 
 export default function Routing() {
     return (
@@ -15,6 +19,10 @@ export default function Routing() {
               <Route index element={<Home/>} />
               <Route path="products" element={<Catalog/>} />
               <Route path="register" element={<Register/>} />
+              <Route path="vendor" element={<Vendor/>} />
+              <Route path="vendor/products" element={<VendorProduct/>} />
+              <Route path="vendor/profile" element={<VendorProfile/>} />
+              <Route path="vendor/transactions" element={<VendorTransactions/>} />
               <Route path="login" element={<Login/>} />
               <Route path="reset" element={<Reset/>} />
             </Route>
